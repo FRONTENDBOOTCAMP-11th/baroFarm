2c93617fcc8a567834646ba6c7247edbaa666c24
eunjinim <eunjee2058@gmail.com>
2024-12-23 20:34:39 +0900

    feat: 로그인 페이지 마크업 및 스타일링

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,24 +1,24 @@
-import { lazy } from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { lazy } from "react"
+import { createBrowserRouter } from "react-router-dom"
 
-const BoardDetailPage = lazy(() => import("@pages/board/BoardDetailPage"));
-const BoardNewPage = lazy(() => import("@pages/board/BoardNewPage"));
-const BoardPage = lazy(() => import("@pages/board/BoardPage"));
-const CategoryPage = lazy(() => import("@pages/market/CategoryPage"));
-const ProductDetailPage = lazy(() => import("@pages/market/ProductDetailPage"));
-const ProductNewPage = lazy(() => import("@pages/market/ProductNewPage"));
-const ProductReviewPage = lazy(() => import("@pages/market/ProductReviewPage"));
-const BookmarkPage = lazy(() => import("@pages/user/BookmarkPage"));
-const MyPage = lazy(() => import("@pages/user/MyPage"));
-const ProfilePage = lazy(() => import("@pages/user/ProfilePage"));
-const MenuPage = lazy(() => import("@pages/market/MenuPage"));
-const CartPage = lazy(() => import("@pages/market/CartPage"));
-const SearchPage = lazy(() => import("@pages/market/SearchPage"));
-const SearchResultsPage = lazy(() => import("@pages/market/SearchResultsPage"));
-const MainPage = lazy(() => import("@pages/index"));
-const LoginPage = lazy(() => import("@pages/user/LoginPage"));
-const SignupPage = lazy(() => import("@pages/user/SignupPage"));
-const Layout = lazy(() => import("@components/layout"));
+const BoardDetailPage = lazy(() => import("@pages/board/BoardDetailPage"))
+const BoardNewPage = lazy(() => import("@pages/board/BoardNewPage"))
+const BoardPage = lazy(() => import("@pages/board/BoardPage"))
+const CategoryPage = lazy(() => import("@pages/market/CategoryPage"))
+const ProductDetailPage = lazy(() => import("@pages/market/ProductDetailPage"))
+const ProductNewPage = lazy(() => import("@pages/market/ProductNewPage"))
+const ProductReviewPage = lazy(() => import("@pages/market/ProductReviewPage"))
+const BookmarkPage = lazy(() => import("@pages/user/BookmarkPage"))
+const MyPage = lazy(() => import("@pages/user/MyPage"))
+const ProfilePage = lazy(() => import("@pages/user/ProfilePage"))
+const MenuPage = lazy(() => import("@pages/market/MenuPage"))
+const CartPage = lazy(() => import("@pages/market/CartPage"))
+const SearchPage = lazy(() => import("@pages/market/SearchPage"))
+const SearchResultsPage = lazy(() => import("@pages/market/SearchResultsPage"))
+const MainPage = lazy(() => import("@pages/index"))
+const LoginPage = lazy(() => import("@pages/user/LoginPage"))
+const SignupPage = lazy(() => import("@pages/user/SignupPage"))
+const Layout = lazy(() => import("@components/layout"))
 
 const router = createBrowserRouter(
   [
@@ -80,6 +80,6 @@ const router = createBrowserRouter(
       v7_skipActionErrorRevalidation: true,
     },
   }
-);
+)
 
-export default router;
+export default router
