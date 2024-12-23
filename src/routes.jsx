04fbdb5184dd065d4821ac76c464b1188ff7f3c5
Taiho1998 <tgo12323@gmail.com>
2024-12-23 11:11:10 +0900

    이미지 폴더 생성 및 아이콘 추가, App.jsx 복구

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,15 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "@components/layout";
+
+const Layout = lazy(() => import("@components/layout"));
+const MenuPage = lazy(() => import("@pages/market/MenuPage"));
+const CartPage = lazy(() => import("@pages/market/CartPage"));
+const SearchPage = lazy(() => import("@pages/market/SearchPage"));
+const SearchResultPage = lazy(() => import("@pages/market/SearchResultPage"));
+// const ErrorPage = lazy(() => import("@pages/ErrorPage"));
+const MainPage = lazy(() => import("@pages/index"));
+const LoginPage = lazy(() => import("@pages/user/Login"));
+const SignupPage = lazy(() => import("@pages/user/Signup"));
 
 const router = createBrowserRouter(
   [
@@ -7,6 +17,12 @@ const router = createBrowserRouter(
       path: "/",
       element: <Layout />,
       children: [
+        { index: true, element: <MainPage /> },
+        { path: "/menu", element: <MenuPage /> },
+        { path: "pages/market/CartPage", element: <CartPage /> },
+        { path: "pages/market/SearchPage", element: <SearchPage /> },
+        { path: "user/signup", element: <SignupPage /> },
+        { path: "user/login", element: <LoginPage /> },
       ],
     },
   ],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['index.html', './src/**/*.{js,jsx}'],
+  content: ["index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      colors : {
-        "white" : "#FFFFFF",
-        "gray1" : "#F5F5F5",
-        "gray2" : "#D9D9D9",
-        "gray3" : "#C4C4C4",
-        "gray4" : "#8E8E8E",
-        "gray5" : "#5F5F5F",
-        "black" : "#000000",
-        "bg-primary" : "#8EB486",
-        "btn-primary" : "#72BF78",
-        "primary-darkmode" : "#C2FFC7",
-        "red" : "#FF0000",
-        "yellow" : "#FEE500",
+      colors: {
+        white: "#FFFFFF",
+        gray1: "#F5F5F5",
+        gray2: "#D9D9D9",
+        gray3: "#C4C4C4",
+        gray4: "#8E8E8E",
+        gray5: "#5F5F5F",
+        black: "#000000",
+        "bg-primary": "#8EB486",
+        "btn-primary": "#72BF78",
+        "primary-darkmode": "#C2FFC7",
+        red: "#FF0000",
+        yellow: "#FEE500",
       },
       boxShadow: {
         top: "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
