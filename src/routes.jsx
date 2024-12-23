4780846c825c2af7a77da3e795d97fb077afe1cd
Taiho1998 <tgo12323@gmail.com>
2024-12-23 10:18:12 +0900

    routes.jsx 내용 변경

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "@components/layout";
 
 const router = createBrowserRouter(
   [
     {
       path: "/",
-      element: <></>,
+      element: <Layout />,
       children: [
       ],
     },
