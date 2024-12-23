3e4fb8aeeb1ca02dfae021973cbd2c58c32a5bc6
Taiho1998 <tgo12323@gmail.com>
2024-12-23 13:37:58 +0900

    layout/index.html 수정

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="w-[390px] mx-auto">
       <Header />
       <Outlet />
       <Footer />
