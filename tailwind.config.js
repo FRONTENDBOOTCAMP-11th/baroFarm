6d07ff8b947099fdf9f1eed303d435b27ab4b40b
Taiho1998 <tgo12323@gmail.com>
2024-12-23 15:20:40 +0900

    마크업 레이어 작성

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,8 @@ export default {
         top: "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
       },
       fontFamily: {
-        pre: ['Pretendard']
-      }
+        pre: ["Pretendard"],
+      },
     },
   },
   plugins: [],
