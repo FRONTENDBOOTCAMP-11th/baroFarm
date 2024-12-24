48d6fdeb3dacb70bd8215a89705f666af8b8cf95
Taiho1998 <tgo12323@gmail.com>
2024-12-24 18:54:21 +0900

    feat: 게시판 기본 구조

--- src/pages/board/BoardPage.jsx
+++ src/pages/board/BoardPage.jsx
@@ -1,3 +1,20 @@
 export default function BoardPage() {
-  return <div>BoardPage</div>;
+  return (
+    <div className="bg-red-300 max-w-[390px]">
+      <div className="flex flex-row h-[50px] ml-[-20px] w-[390px] bg-blue-100">
+        <div className="flex w-[120px] justify-center items-center border-r-[1px] border-gray3/50">
+          자유
+        </div>
+        <div className="flex w-[120px] justify-center items-center border-r-[1px] border-gray3/50">
+          정보 공유
+        </div>
+      </div>
+      <div>
+        <div className="h-[7px] bg-gray1 ml-[-20px] w-[390px]"></div>
+        <div className="flex flex-row">user info</div>
+        <div>post content</div>
+        <div>media</div>
+      </div>
+    </div>
+  );
 }
