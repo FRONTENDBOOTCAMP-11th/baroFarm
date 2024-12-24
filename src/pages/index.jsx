c13cd22973a2909b2d8c9b2b6ad1f8d7990b4773
Jinsu <injnamek@gmail.com>
2024-12-24 21:19:33 +0900

    feature: 인기 상품 UI 구현

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,54 @@ import HeaderIcon from "@components/HeaderIcon";
 import { useEffect } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 
+// image
+import productImage1 from "/images/Sample1.svg";
+import productImage2 from "/images/Sample2.svg";
+import Product from "@components/Product";
+
+const productsData = [
+  {
+    id: 1,
+    image: productImage1,
+    title: "온도감",
+    content: "촉촉함이 다른 카스테라 5종...",
+    sale: "92%",
+    price: "14,900원",
+    rate: "⭐️ 4.9",
+    review: "(2,210)",
+  },
+  {
+    id: 2,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "12%",
+    price: "24,900원",
+    rate: "⭐️ 3.9",
+    review: "(6,210)",
+  },
+  {
+    id: 3,
+    image: productImage1,
+    title: "햄스터",
+    content: "햄스터 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+  {
+    id: 4,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+];
+
 export default function MainPage() {
   // Outlet 컴포넌트로 전달받은 props.setHeadetContents 접근
   const { setHeadetContents } = useOutletContext();
@@ -65,9 +113,14 @@ export default function MainPage() {
         </div>
       </section>
       <section className="px-5 mb-4">
-        <h2 className="text-xl mb-3">
+        <h2 className="text-xl">
           지금 최고 <span className="font-bold">인기 상품!</span>
         </h2>
+        <div className="flex flex-wrap justify-between">
+          {productsData.map((product) => (
+            <Product key={product.id} {...product} />
+          ))}
+        </div>
       </section>
     </div>
   );
