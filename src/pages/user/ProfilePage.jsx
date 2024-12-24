5e9a5fec541d11a945be91edd6c2eda98db7e6b4
Taiho1998 <tgo12323@gmail.com>
2024-12-24 11:48:42 +0900

    feat: 프로필 페이지 마크업 작성

--- src/pages/user/ProfilePage.jsx
+++ src/pages/user/ProfilePage.jsx
@@ -1,76 +1,74 @@
-import Footer from "@components/layout/Footer";
-
 export default function ProfilePage() {
   return (
-    <div className="max-w-[390px] mx-auto">
-      <header className="h-[70px]">헤더</header>
-      <div className="pt-[18px]">
-        <div className="h-[144px] px-[20px]">
-          <div>
-            <div className="flex flex-row items-center">
-              <img src="" className="mr-5 w-[49px] h-[50px]" loading="lazy" />
-              <div>
-                <p className="text-gray2/50 text-[12px] leading-[14px]">
-                  판매자
-                </p>
-                <h2 className="text-[16px] leading-[18px] mt-[4px]">
-                  온도감님! 어서오세요
-                </h2>
-              </div>
-              <a className="flex ml-auto h-fit items-center text-gray2/50 text-[14px]">
-                프로필 보기
-                <img
-                  src="../../../../public/icons/icon_forward.svg"
-                  className="h-[16px] ml-[11px]"
-                  alt="profileDetail icon"
-                />
-              </a>
+    <div className="pt-[18px] max-w-[390px] mx-auto mb-[70px]">
+      <div className="h-[144px]">
+        <div>
+          <div className="flex flex-row items-center">
+            <img
+              src="../../../../public/images/ProfileImage_Sample.svg"
+              className="mr-5 w-[49px] h-[50px]"
+              loading="lazy"
+            />
+            <div>
+              <p className="text-gray5/50 text-[12px] leading-[14px]">판매자</p>
+              <h2 className="text-[16px] leading-[18px] mt-[4px]">
+                온도감님! 어서오세요
+              </h2>
             </div>
-            <div className="flex items-center border-gray3 border-t-[1px] mt-[14px] h-[58px]">
-              <button className="flex-1 text-center h-[50px] border-r-[1px]">
-                구매 내역
-              </button>
-              <button className="flex-1 text-center h-[50px] border-r-[1px]">
-                판매 내역
-              </button>
-              <button className="flex-1 text-center h-[50px]">
-                작성한 글 <span className="text-btn-primary">3건</span>
-              </button>
-            </div>
+            <a className="flex ml-auto h-fit items-center text-[14px]">
+              프로필 보기
+              <img
+                src="../../../../public/icons/icon_forward.svg"
+                className="h-4 ml-2"
+                alt="profileDetail icon"
+              />
+            </a>
+          </div>
+          <div className="flex items-center border-t-[1px] border-gray2 h-[58px] mt-[16px]">
+            <button className="flex-1 text-center h-[50px] border-r-[1px] border-gray2">
+              구매 내역
+            </button>
+            <button className="flex-1 text-center h-[50px] border-r-[1px] border-gray2">
+              판매 내역
+            </button>
+            <button className="flex-1 text-center h-[50px]">
+              작성한 글 <span className="text-btn-primary">3건</span>
+            </button>
           </div>
         </div>
-        <div className="h-[152px] px-[20px] pt-[18px] border-t-[7px] border-gray2">
-          <h2 className="text-base leading-[19px]">구매 정보</h2>
-          <a className="flex items-center text-gray2/50 text-[14px] mt-[27px]">
-            최근 본 상품
-            <img
-              src="../../../../public/icons/icon_forward.svg"
-              className="h-[16px] ml-auto"
-              alt="profileDetail icon"
-            />
-          </a>
-          <a className="flex items-center text-gray2/50 text-[14px] mt-[24px]">
-            찜한 상품
-            <img
-              src="../../../../public/icons/icon_forward.svg"
-              className="h-[16px] ml-auto"
-              alt="profileDetail icon"
-            />
-          </a>
-        </div>
-        <div className="h-[152px] px-[20px] pt-[18px] border-t-[7px] border-gray2">
-          <h2 className="text-base leading-[19px]">판매 정보</h2>
-          <a className="flex items-center text-gray2/50 text-[14px] mt-[27px]">
-            상품 등록
-            <img
-              src="../../../../public/icons/icon_forward.svg"
-              className="h-[16px] ml-auto"
-              alt="profileDetail icon"
-            />
-          </a>
-        </div>
       </div>
-      <Footer />
+      <div className="h-[7px] w-[390px] bg-gray1 mx-[-20px]"></div>
+      <div className="h-[152px] pt-[18px]">
+        <h2 className="text-base leading-[19px]">구매 정보</h2>
+        <a className="flex items-center text-[14px] mt-[27px]">
+          최근 본 상품
+          <img
+            src="../../../../public/icons/icon_forward.svg"
+            className="h-[16px] ml-auto"
+            alt="recentProduct icon"
+          />
+        </a>
+        <a className="flex items-center text-[14px] mt-[24px]">
+          찜한 상품
+          <img
+            src="../../../../public/icons/icon_forward.svg"
+            className="h-[16px] ml-auto"
+            alt="likedProduct icon"
+          />
+        </a>
+      </div>
+      <div className="h-[7px] w-[390px] bg-gray1 mx-[-20px]"></div>
+      <div className="h-[152px] pt-[18px] ">
+        <h2 className="text-base leading-[19px]">판매 정보</h2>
+        <a className="flex items-center text-[14px] mt-[27px] mb-[24px]">
+          상품 등록
+          <img
+            src="../../../../public/icons/icon_forward.svg"
+            className="h-[16px] ml-auto"
+            alt="addProduct icon"
+          />
+        </a>
+      </div>
     </div>
   );
 }
