23eee97d9a0e25a8a75c22978787e380af84c34c
Taiho1998 <tgo12323@gmail.com>
2024-12-26 06:09:37 +0900

    feat: 새 글 쓰기 버튼 링크 추가

--- src/pages/board/BoardPage.jsx
+++ src/pages/board/BoardPage.jsx
@@ -1,8 +1,9 @@
 import BoardPageDetail from "@pages/board/BoardPageDetail";
+import { Link } from "react-router-dom";
 
 export default function BoardPage() {
   return (
-    <div className="relative">
+    <div className="relative mx-5">
       <div className="flex flex-row h-[50px] mx-[-20px] max-w-[calc(100%+40px)] ">
         <div className="flex w-full max-w-[120px] justify-center text-sm items-center border-r-[1px] border-gray3/50">
           자유
@@ -13,6 +14,12 @@ export default function BoardPage() {
       </div>
       <BoardPageDetail />
       <BoardPageDetail />
+      <Link
+        to="new"
+        className="fixed right-[calc(50%-195px+20px)] bottom-[120px] w-[50px] h-[50px] rounded-full bg-gray5"
+      >
+        <img src="/icons/icon_plus_2.svg" className="w-full h-full" />
+      </Link>
     </div>
   );
 }
