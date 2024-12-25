32e4bbb89b62bcf6f1a9ed4c590e65ae00b55634
Taiho1998 <tgo12323@gmail.com>
2024-12-26 01:05:15 +0900

    feat: 유저 정보란 및 게시글 내용 영역 스타일 작성

--- src/pages/board/BoardPage.jsx
+++ src/pages/board/BoardPage.jsx
@@ -2,18 +2,29 @@ export default function BoardPage() {
   return (
     <div className="bg-red-300 max-w-[390px]">
       <div className="flex flex-row h-[50px] ml-[-20px] w-[390px] bg-blue-100">
-        <div className="flex w-[120px] justify-center items-center border-r-[1px] border-gray3/50">
+        <div className="flex w-[120px] justify-center text-sm items-center border-r-[1px] border-gray3/50">
           자유
         </div>
-        <div className="flex w-[120px] justify-center items-center border-r-[1px] border-gray3/50">
+        <div className="flex w-[120px] justify-center text-sm items-center border-r-[1px] border-gray3/50">
           정보 공유
         </div>
       </div>
       <div>
         <div className="h-[7px] bg-gray1 ml-[-20px] w-[390px]"></div>
-        <div className="flex flex-row">user info</div>
-        <div>post content</div>
-        <div>media</div>
+        <div className="flex flex-row mt-5">
+          <img
+            src="../../../../public/images/ProfileImage_Sample.svg"
+            alt="ProfileImage"
+            className="w-6 h-6"
+          />
+          <span className="mx-[5px] text-sm">온도감</span>
+          <span className="text-[10px] mt-[6px] text-gray4">16분 전</span>
+          <span className="ml-auto text-xs">댓글 1개</span>
+        </div>
+        <div className="mx-[5px] mt-[30px]">
+          요즘 토마토가 또 철이네요~ 우리 집에서 기른 토마토로 만든...
+        </div>
+        <div className="mt-10">media</div>
       </div>
     </div>
   );
