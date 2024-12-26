60e3c9a2b6288c75ebca0b6d5c576839efa4b70e
Jinsu <injnamek@gmail.com>
2024-12-26 11:43:49 +0900

    feat: 장바구니 담은 상품에 따른 동적 렌더링

--- src/pages/market/CartPage.jsx
+++ src/pages/market/CartPage.jsx
@@ -73,10 +73,17 @@ const DUMMY_CARTS_ITEMS = {
   },
 };
 
+const DUMMY_EMPTY_CARTS = {
+  ok: 1,
+  item: [],
+};
+
 export default function CartPage() {
   const cartItems = DUMMY_CARTS_ITEMS.item.map((item) => (
     <CartItem key={item._id} {...item.product} />
   ));
+
+  console.log(cartItems);
 
   const totalShippingFees =
     DUMMY_CARTS_ITEMS.cost.shippingFees ===
@@ -86,35 +93,48 @@ export default function CartPage() {
 
   return (
     <div className="mb-[100px]">
-      <section className="py-[14px] flex gap-[6px] items-center border-b border-gray2">
-        <input type="checkbox" id="checkAll" />
-        <label htmlFor="checkAll">전체 선택 (1/2)</label>
-        <button className="ml-auto bg-gray3 px-3 py-1">삭제</button>
-      </section>
-      <section className="px-5 pb-4 border-b-4 border-gray2">
-        {cartItems}
-      </section>
-      <section className="px-5 py-3">
-        <div className="border-b border-gray2">
-          <div className="text-xs flex justify-between mb-3">
-            <span className="text-gray4">총 상품 금액</span>
-            <span>{DUMMY_CARTS_ITEMS.cost.products.toLocaleString()}원</span>
-          </div>
-          <div className="text-xs flex justify-between mb-3">
-            <span className="text-gray4">배송비</span>
-            <span>{totalShippingFees}</span>
-          </div>
-        </div>
-        <div className="flex justify-between mb-3 py-3 text-[16px] font-bold">
-          <span>총 결제 금액</span>
-          <span>{DUMMY_CARTS_ITEMS.cost.total.toLocaleString()}원</span>
-        </div>
-      </section>
-      <section className="px-5 py-8 bg-gray1 shadow-top">
-        <button className="bg-btn-primary py-3 w-full text-white text-xl font-bold rounded-lg">
-          {DUMMY_CARTS_ITEMS.cost.total.toLocaleString()}원 결제하기
-        </button>
-      </section>
+      {cartItems.length > 0 ? (
+        <>
+          <section className="py-[14px] flex gap-[6px] items-center border-b border-gray2">
+            <input type="checkbox" id="checkAll" />
+            <label htmlFor="checkAll">전체 선택 (1/2)</label>
+            <button className="ml-auto bg-gray3 px-3 py-1">삭제</button>
+          </section>
+          <section className="px-5 pb-4 border-b-4 border-gray2">
+            {cartItems}
+          </section>
+          <section className="px-5 py-3">
+            <div className="border-b border-gray2">
+              <div className="text-xs flex justify-between mb-3">
+                <span className="text-gray4">총 상품 금액</span>
+                <span>
+                  {DUMMY_CARTS_ITEMS.cost.products.toLocaleString()}원
+                </span>
+              </div>
+              <div className="text-xs flex justify-between mb-3">
+                <span className="text-gray4">배송비</span>
+                <span>{totalShippingFees}</span>
+              </div>
+            </div>
+            <div className="flex justify-between mb-3 py-3 text-[16px] font-bold">
+              <span>총 결제 금액</span>
+              <span>{DUMMY_CARTS_ITEMS.cost.total.toLocaleString()}원</span>
+            </div>
+          </section>
+          <section className="px-5 py-8 bg-gray1 shadow-top">
+            <button className="bg-btn-primary py-3 w-full text-white text-xl font-bold rounded-lg">
+              {DUMMY_CARTS_ITEMS.cost.total.toLocaleString()}원 결제하기
+            </button>
+          </section>
+        </>
+      ) : (
+        <>
+          <section className="pt-[100px] flex flex-col gap-[10px] items-center text-[14px]">
+            <span className="text-gray4">담은 상품이 없습니다.</span>
+            <span className="text-bg-primary underline">쇼핑하러 가기</span>
+          </section>
+        </>
+      )}
     </div>
   );
 }
