24ecdfc9c867655364f8789f3d3aed8bcd272fcb
Saebom <spring180@naver.com>
2024-12-26 14:42:02 +0900

    feat: 네비게이션 조건부 렌더링 (#39)

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,11 +1,15 @@
 import Footer from "@components/layout/Footer";
 import Header from "@components/layout/Header";
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function Layout() {
   // 헤더 title을 동적으로 렌더링하기 위한 상태 관리
   const [headerContents, setHeaderContents] = useState({});
+  const { pathname } = useLocation();
+
+  const showFooter =
+    !pathname.includes("product") && !pathname.includes("cart");
 
   return (
     <div className="max-w-[390px] mx-auto">
@@ -14,7 +18,7 @@ export default function Layout() {
         {/* Outlet에 데이터를 전달하려면 반드시 context라는 이름으로 데이터를 제공해야 함 */}
         <Outlet context={{ setHeaderContents }} />
       </main>
-      <Footer />
+      {showFooter && <Footer />}
     </div>
   );
 }
