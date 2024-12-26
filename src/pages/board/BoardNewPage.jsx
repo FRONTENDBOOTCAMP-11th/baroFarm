730038895c802a543480a203ee9fc28122f653a4
Taiho1998 <tgo12323@gmail.com>
2024-12-26 13:58:39 +0900

    feature/header 컴포넌트 설정

--- src/pages/board/BoardNewPage.jsx
+++ src/pages/board/BoardNewPage.jsx
@@ -1,4 +1,24 @@
+import HeaderIcon from "@components/HeaderIcon";
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
 export default function BoardNewPage() {
+  const { setHeaderContents } = useOutletContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setHeaderContents({
+      leftChild: <HeaderIcon name="back" onClick={() => navigate(-1)} />,
+      title: "새 글 작성",
+      rightChild: (
+        <>
+          {/* 등록시 게시판으로 이동 */}
+          <HeaderIcon name="register" onClick={() => navigate("/board")} />
+        </>
+      ),
+    });
+  }, []);
+
   return (
     <div className="relative mx-5">
       <div className="flex flex-row mt-5 items-center">
@@ -16,13 +36,13 @@ export default function BoardNewPage() {
         placeholder="본문 내용을 입력해주세요."
       ></textarea>
       <br />
-      <label>이미지 첨부</label>
+      <label className="font-bold">이미지 첨부</label>
       <input
         type="file"
         id="attach"
         accept="image/*"
         placeholder="이미지를 선택하세요"
-        className="w-full px-3 py-2 border rounded-lg dark:bg-gray-700"
+        className="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 mt-[10px]"
         name="attach"
       />
     </div>
