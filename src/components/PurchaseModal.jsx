0f0d24897da4a097ac01930341d76c277ed3cda8
Saebom <spring180@naver.com>
2024-12-27 16:48:42 +0900

    fix: 상품 구매 모달 스크롤 문제 수정 (#41)

--- src/components/PurchaseModal.jsx
+++ src/components/PurchaseModal.jsx
@@ -1,37 +1,48 @@
 import { useRef, forwardRef, useImperativeHandle, useState } from "react";
 import { createPortal } from "react-dom";
-
 import downIcon from "/icons/icon_down_thin.svg";
-import cartIcon from "/icons/icon_cart_modal.svg";
 
 const PurchaseModal = forwardRef(({ children }, ref) => {
-  const [isOpen, setIsOpen] = useState(false);
   const dialogRef = useRef();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const open = () => {
+    setIsVisible(true);
+    dialogRef.current.showModal();
+  };
+
+  const close = () => {
+    setIsVisible(false);
+    dialogRef.current.close();
+  };
 
   useImperativeHandle(ref, () => ({
-    open: () => {
-      setIsOpen(true);
-      dialogRef.current.showModal();
-    },
-    close: () => {
-      setIsOpen(false);
-      dialogRef.current.close();
-    },
+    open,
+    close,
   }));
 
-  return createPortal(
-    <dialog
-      ref={dialogRef}
-      className="w-[390px] max-w-none backdrop:bg-black/50 border-t-[3px] border-btn-primary flex flex-col gap-8 p-5 pt-0 mb-0"
-    >
-      <form method="dialog">
-        <button className="px-7 py-2 bg-btn-primary rounded-b-[10px] absolute top-0 left-1/2 transform -translate-x-1/2">
-          <img src={downIcon} className="w-6" />
-        </button>
-      </form>
-      {children}
-    </dialog>,
-    document.getElementById("modal-root")
+  return (
+    <>
+      {isVisible &&
+        createPortal(
+          <dialog
+            ref={dialogRef}
+            className="w-[390px] max-w-none backdrop:bg-black/50 border-t-[3px] border-btn-primary flex flex-col gap-8 p-5 pt-0 mb-0 fixed inset-0"
+          >
+            <form method="dialog">
+              <button
+                type="button"
+                className="px-7 py-2 bg-btn-primary rounded-b-[10px] absolute top-0 left-1/2 transform -translate-x-1/2"
+                onClick={close}
+              >
+                <img src={downIcon} className="w-6" />
+              </button>
+            </form>
+            {children}
+          </dialog>,
+          document.getElementById("modal-root")
+        )}
+    </>
   );
 });
 
