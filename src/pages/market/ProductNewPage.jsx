6e74006781e3f0fb116b7ce51669aa48bb04c62c
Taiho1998 <tgo12323@gmail.com>
2024-12-30 00:34:49 +0900

    feature/카테고리 영역 select 태그로 수정, option 추가

--- src/pages/market/ProductNewPage.jsx
+++ src/pages/market/ProductNewPage.jsx
@@ -14,20 +14,46 @@ export default function ProductNewPage() {
   }, []);
 
   return (
-    <form className="mx-5 py-5">
-      <input type="text" className="bg-gray2/20 w-full h-[42px]" />
+    <form className="mx-5 py-5" action="#">
+      <input
+        type="text"
+        className="bg-gray2/20 w-full h-[50px] px-4"
+        placeholder="상품명을 입력해주세요."
+      />
       <br />
-      <input type="text" className="bg-gray2/20 w-full h-[42px] mt-[25px]" />
+      <select
+        type="text"
+        className="bg-gray2/20 w-full h-[50px] mt-[25px] px-4"
+        placeholder="본문 내용을 입력해주세요."
+      >
+        <option value="select">카테고리</option>
+        <option value="">과일</option>
+        <option value="">채소</option>
+        <option value="">김치</option>
+        <option value="">축산물</option>
+        <option value="">수산물</option>
+        <option value="">간편식</option>
+        <option value="">쌀</option>
+        <option value="">떡</option>
+      </select>
       <textarea
         name="contents"
         id="contents"
-        className="w-full my-[25px] h-[200px] p-3 border-gray3 border-[1px] bg-gray2/20"
+        className="w-full my-[25px] h-[200px] p-3 border-gray3 border-[1px] bg-gray2/20 "
         placeholder="본문 내용을 입력해주세요."
       ></textarea>
-      <label className="font-bold">이미지 첨부</label>
-      <input type="text" className="bg-gray2/20 w-full h-[42px]" /> <br />
-      <label className="font-bold">이미지 첨부</label>
-      <input type="text" className="bg-gray2/20 w-full h-[42px]" /> <br />
+      <label className="font-bold">판매 희망 가격</label>
+      <input
+        type="text"
+        className="bg-gray2/20 w-full h-[50px] mb-[25px] px-4"
+      />
+      <br />
+      <label className="font-bold">판매 희망 개수</label>
+      <input
+        type="text"
+        className="bg-gray2/20 w-full h-[50px] mb-[25px] px-4"
+      />
+      <br />
       <label className="font-bold">이미지 첨부</label>
       <input
         type="file"
