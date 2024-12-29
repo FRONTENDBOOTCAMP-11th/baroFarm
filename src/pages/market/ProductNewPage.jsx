576a4df3556dc114b261a3a573a266351f6ebd56
Taiho1998 <tgo12323@gmail.com>
2024-12-30 01:58:09 +0900

    feature/select 태그의 임시 value 추가, spinner의 style 추가 및 onClick & onBlur 이벤트 추가

--- src/pages/market/ProductNewPage.jsx
+++ src/pages/market/ProductNewPage.jsx
@@ -1,6 +1,6 @@
 import Button from "@components/Button";
 import HeaderIcon from "@components/HeaderIcon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 
 export default function ProductNewPage() {
@@ -10,9 +10,21 @@ export default function ProductNewPage() {
     setHeaderContents({
       leftChild: <HeaderIcon name="back" onClick={() => navigate(-1)} />,
       title: "상품 등록",
+      rightChild: <Button>등록</Button>,
     });
   }, []);
 
+  const [price, setPrice] = useState(0);
+  const [tag, setTag] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleBlur = () => {
+    setIsOpen(false); // 드롭다운 닫을 때 상태 초기화
+  };
   return (
     <form className="mx-5 py-5" action="#">
       <input
@@ -21,37 +33,62 @@ export default function ProductNewPage() {
         placeholder="상품명을 입력해주세요."
       />
       <br />
-      <select
-        type="text"
-        className="bg-gray2/20 w-full h-[50px] mt-[25px] px-4"
-        placeholder="본문 내용을 입력해주세요."
-      >
-        <option value="select">카테고리</option>
-        <option value="">과일</option>
-        <option value="">채소</option>
-        <option value="">김치</option>
-        <option value="">축산물</option>
-        <option value="">수산물</option>
-        <option value="">간편식</option>
-        <option value="">쌀</option>
-        <option value="">떡</option>
-      </select>
+      <div className="relative w-full">
+        <select
+          type="text"
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+          onClick={toggleDropdown}
+          onBlur={handleBlur}
+          className={`bg-gray2/20 w-full h-[50px] mt-[25px] px-4 appearance-none`}
+        >
+          <option value="select">카테고리</option>
+          <option value="fruit">과일</option>
+          <option value="vegetable">채소</option>
+          <option value="kimchi">김치</option>
+          <option value="livestock">축산물</option>
+          <option value="seafood">수산물</option>
+          <option value="ReadyToEat">간편식</option>
+          <option value="Rice">쌀</option>
+          <option value="RiceCake">떡</option>
+        </select>
+        <span
+          className={`absolute right-4 top-[42px] ${
+            isOpen ? "rotate-90" : "-rotate-90"
+          }`}
+        >
+          <img
+            src="/icons/icon_back_thin.svg"
+            alt="dropdown arrow"
+            className="w-4 h-4"
+          />
+        </span>
+      </div>
       <textarea
         name="contents"
         id="contents"
-        className="w-full my-[25px] h-[200px] p-3 border-gray3 border-[1px] bg-gray2/20 "
+        className="w-full my-[25px] h-[200px] p-3 border-gray3 border-[1px] bg-gray2/20"
         placeholder="본문 내용을 입력해주세요."
       ></textarea>
       <label className="font-bold">판매 희망 가격</label>
-      <input
-        type="text"
-        className="bg-gray2/20 w-full h-[50px] mb-[25px] px-4"
-      />
+      <div className="relative w-full">
+        <input
+          type="text"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          className="bg-gray2/20 w-full h-[50px] pr-12 px-4"
+          placeholder="가격을 입력하세요"
+        />
+        <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600">
+          원
+        </span>
+      </div>
       <br />
       <label className="font-bold">판매 희망 개수</label>
       <input
         type="text"
         className="bg-gray2/20 w-full h-[50px] mb-[25px] px-4"
+        placeholder="판매 개수를 입력하세요"
       />
       <br />
       <label className="font-bold">이미지 첨부</label>
@@ -63,7 +100,6 @@ export default function ProductNewPage() {
         className="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 mt-[10px]"
         name="attach"
       />
-      <Button>등록</Button>
     </form>
   );
 }
