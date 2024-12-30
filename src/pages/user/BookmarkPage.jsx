405fabaa861905aebe42a04ed120ca97e9e045ac
Saebom <spring180@naver.com>
2024-12-30 17:08:46 +0900

    feat: Products 컴포넌트 추가 (#70)

--- src/pages/user/BookmarkPage.jsx
+++ src/pages/user/BookmarkPage.jsx
@@ -3,6 +3,73 @@ import { useNavigate, useOutletContext } from "react-router-dom";
 
 import HeaderIcon from "@components/HeaderIcon";
 
+import productImage1 from "/images/Sample1.svg";
+import productImage2 from "/images/Sample2.svg";
+import Products from "@components/Products";
+
+const productsData = [
+  {
+    id: 1,
+    image: productImage1,
+    title: "온도감",
+    content: "촉촉함이 다른 카스테라 5종...",
+    sale: "92%",
+    price: "14,900원",
+    rate: "⭐️ 4.9",
+    review: "(2,210)",
+  },
+  {
+    id: 2,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "12%",
+    price: "24,900원",
+    rate: "⭐️ 3.9",
+    review: "(6,210)",
+  },
+  {
+    id: 3,
+    image: productImage1,
+    title: "햄스터",
+    content: "햄스터 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+  {
+    id: 4,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+  {
+    id: 5,
+    image: productImage1,
+    title: "햄스터",
+    content: "햄스터 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+  {
+    id: 6,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+];
+
 export default function BookmarkPage() {
   const { setHeaderContents } = useOutletContext();
   const navigate = useNavigate();
@@ -19,5 +86,5 @@ export default function BookmarkPage() {
     });
   }, []);
 
-  return <></>;
+  return <Products productsData={productsData} />;
 }
