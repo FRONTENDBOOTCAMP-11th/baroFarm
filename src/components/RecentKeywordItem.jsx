906bc25dd957031b80a9a630bd4066b905c05437
eunjinim <eunjee2058@gmail.com>
2024-12-30 09:32:04 +0900

    refactor: 최근 검색어 컴포넌트로 분리

---
+++ src/components/RecentKeywordItem.jsx
@@ -0,0 +1,21 @@
+export default function RecentKeywordItem() {
+  return (
+    <>
+      <li className="bg-gray2 px-3 rounded-xl">
+        <a className="hover:font-semibold" href="#">
+          귤
+        </a>
+      </li>
+      <li className="bg-gray2 px-3 rounded-xl">
+        <a className="hover:font-semibold" href="#">
+          갈치
+        </a>
+      </li>
+      <li className="bg-gray2 px-3 rounded-xl">
+        <a className="hover:font-semibold" href="#">
+          고랭지배추
+        </a>
+      </li>
+    </>
+  );
+}
