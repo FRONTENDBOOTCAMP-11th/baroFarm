4743719f949ab7973ce8fac40ad01dcf02377929
eunjinim <eunjee2058@gmail.com>
2024-12-30 20:55:41 +0900

    refactor: 상품 목록 컴포넌트 변경 (Product → Products)

--- src/pages/market/SearchResultsPage.jsx
+++ src/pages/market/SearchResultsPage.jsx
@@ -4,53 +4,53 @@ import { useNavigate, useOutletContext } from "react-router-dom";
 
 // 컴포넌트
 import HeaderIcon from "@components/HeaderIcon";
-import Product from "@components/Product";
+import Products from "@components/Products";
 
 // 이미지
 import productImage1 from "/images/Sample1.svg";
 import productImage2 from "/images/Sample2.svg";
 
 const productsData = [
-  // {
-  //   id: 1,
-  //   image: productImage1,
-  //   title: "온도감",
-  //   content: "촉촉함이 다른 카스테라 5종...",
-  //   sale: "92%",
-  //   price: "14,900원",
-  //   rate: "⭐️ 4.9",
-  //   review: "(2,210)",
-  // },
-  // {
-  //   id: 2,
-  //   image: productImage2,
-  //   title: "강아지",
-  //   content: "강아지 귀여워",
-  //   sale: "12%",
-  //   price: "24,900원",
-  //   rate: "⭐️ 3.9",
-  //   review: "(6,210)",
-  // },
-  // {
-  //   id: 3,
-  //   image: productImage1,
-  //   title: "햄스터",
-  //   content: "햄스터 귀여워",
-  //   sale: "2%",
-  //   price: "4,900원",
-  //   rate: "⭐️ 0.9",
-  //   review: "(210)",
-  // },
-  // {
-  //   id: 4,
-  //   image: productImage2,
-  //   title: "강아지",
-  //   content: "강아지 귀여워",
-  //   sale: "2%",
-  //   price: "4,900원",
-  //   rate: "⭐️ 0.9",
-  //   review: "(210)",
-  // },
+  {
+    id: 1,
+    image: productImage1,
+    title: "온도감",
+    content: "촉촉함이 다른 카스테라 5종...",
+    sale: "92%",
+    price: "14,900원",
+    rate: "⭐️ 4.9",
+    review: "(2,210)",
+  },
+  {
+    id: 2,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "12%",
+    price: "24,900원",
+    rate: "⭐️ 3.9",
+    review: "(6,210)",
+  },
+  {
+    id: 3,
+    image: productImage1,
+    title: "햄스터",
+    content: "햄스터 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
+  {
+    id: 4,
+    image: productImage2,
+    title: "강아지",
+    content: "강아지 귀여워",
+    sale: "2%",
+    price: "4,900원",
+    rate: "⭐️ 0.9",
+    review: "(210)",
+  },
 ];
 
 export default function SearchResultsPage() {
@@ -94,13 +94,7 @@ export default function SearchResultsPage() {
           <p>다른 검색어로 시도하시거나 맞춤법을 확인해주세요.</p>
         </div>
       ) : (
-        <div className="p-5 mb-4">
-          <div className="flex flex-wrap justify-between">
-            {productsData.map((product) => (
-              <Product key={product.id} {...product} />
-            ))}
-          </div>
-        </div>
+        <Products productsData={productsData} />
       )}
     </div>
   );
