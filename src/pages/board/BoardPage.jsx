72936a122569d987d6c7af6ef7b450083edc29b3
Taiho1998 <tgo12323@gmail.com>
2024-12-31 18:02:05 +0900

    refactor/커뮤니티 소개글 작성

--- src/pages/board/BoardPage.jsx
+++ src/pages/board/BoardPage.jsx
@@ -21,17 +21,23 @@ export default function BoardPage() {
   }, []);
   return (
     <div className="relative mx-5">
-      <div>
-        <p>
-          바로파밍은 모든 이용자들을 위한 소통의 공간입니다. 매너를 지키는
-          바로팜 人이 됩시다!
+      <div className="flex my-2 items-center bg-green1 rounded-md">
+        <img
+          src="/images/BaroFarmLogo.svg"
+          alt="바로팜 로고"
+          className="w-[90px]"
+        />
+        <p className="text-white text-sm break-keep">
+          바로파밍은 모든 이용자들을 위한
+          <span className="text-btn-primary"> 소통</span>의 공간입니다.
+          <br /> 매너를 지키는 바로팜인이 됩시다!
         </p>
       </div>
       <BoardPageDetail />
       <BoardPageDetail />
       <Link
         to="new"
-        className="fixed right-[calc(50%-195px+20px)] bottom-[120px] w-[40px] h-[40px] rounded-full bg-btn-primary"
+        className="fixed right-[calc(50%-195px+20px)] bottom-[120px] w-[40px] h-[40px] rounded-full bg-btn-primary shadow-bottom  "
       >
         <img src="/icons/icon_plus_2.svg" className="w-full h-full" />
       </Link>
