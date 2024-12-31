a6c2f1ac5578d6798eaee5e6e2943fa945fd0da4
eunjinim <eunjee2058@gmail.com>
2024-12-31 14:14:20 +0900

    refactor: 로그인 페이지 수정

--- src/pages/user/LoginPage.jsx
+++ src/pages/user/LoginPage.jsx
@@ -22,33 +22,33 @@ export default function LoginPage() {
 
       {/* 폼 영역 */}
       <form>
-        <div className="border-b-4 border-gray2 mb-8 focus-within:border-b-green1">
+        <div className="border-b-2 border-gray2 mb-8 focus-within:border-b-btn-primary">
           <input type="text" placeholder="이메일" className="placeholder:text-gray4 w-full outline-none" />
         </div>
-        <div className="border-b-4  border-gray2 mb-4 focus-within:border-b-green1">
+        <div className="border-b-2  border-gray2 mb-4 focus-within:border-b-btn-primary">
           <input type="password" placeholder="비밀번호" className="placeholder:text-gray4 w-full outline-none" />
         </div>
-        <label className="mb-8 flex items-center gap-1 cursor-pointer font-medium">
-          <input className="w-5 h-5 mr-1 rounded-full appearance-none bg-gray2 checked:bg-green1" type="checkbox" />
+        <label className="mb-8 flex items-center gap-1 cursor-pointer font-normal">
+          <input className="w-5 h-5 mr-1 rounded-full appearance-none bg-gray2 checked:bg-bg-primary" type="checkbox" />
           <span>로그인 상태 유지</span>
         </label>
       </form>
 
       {/* 버튼 그룹 */}
-      <div className="mb-5 w-[18.75rem] h-[3.25rem] m-auto">
-        <button className="w-[18.75rem] h-[3.25rem] text-center text-xl rounded-full bg-green1 font-semibold">
+      <div className="mb-5 w-full h-[3.25rem] m-auto">
+        <button className="w-full h-[3.25rem] text-center text-xl rounded-full bg-btn-primary font-medium text-white">
           로그인
         </button>
       </div>
-      <div className="mb-10 w-[18.75rem] h-[3.25rem] m-auto">
+      {/* <div className="mb-10 w-[18.75rem] h-[3.25rem] m-auto">
         <button className="w-[18.75rem] h-[3.25rem] text-center text-xl rounded-full bg-yellow1 font-semibold">
           카카오로 3초 만에 로그인
         </button>
-      </div>
-      <div className="mb-5 w-[18.75rem] h-[3.25rem] m-auto">
+      </div> */}
+      <div className="mb-5 w-full h-[3.25rem] m-auto">
         <Link
           to="/users/signup"
-          className="block w-full h-full text-center text-xl rounded-2xl bg-green1 font-semibold leading-[3.25rem]"
+          className="block w-full h-full text-center text-xl rounded-full border-btn-primary border text-btn-primary font-medium leading-[3.25rem] "
         >
           회원가입
         </Link>
