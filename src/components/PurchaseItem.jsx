d6ac7c184fffb061c6e825e665f366d2493556be
봄 <saebom@SaebomucBookPro.localdomain>
2024-12-31 16:50:28 +0900

    feat: 후기 페이지 연결 (#69)

--- src/components/PurchaseItem.jsx
+++ src/components/PurchaseItem.jsx
@@ -2,6 +2,8 @@ import productImage from "/images/Sample1.svg";
 import { Link } from "react-router-dom";
 
 export default function PurchaseItem() {
+  const isReviewed = true;
+
   return (
     <section className="flex gap-5 border-b-[0.5px] border-gray2 py-3">
       <img
@@ -14,10 +16,10 @@ export default function PurchaseItem() {
         <span className="font-semibold">4,280 원</span>
         <span className="ml-4">1개</span>
         <Link
-          to="/product/1/reviews/new"
+          to={isReviewed ? "/product/1/reviews" : "/product/1/reviews/new"}
           className="text-xs absolute bottom-4 right-0 border-b border-gray5 text-gray5 hover:text-btn-primary hover:border-b-btn-primary"
         >
-          후기 작성
+          {isReviewed ? "후기 보기" : "후기 작성"}
         </Link>
       </div>
     </section>
--- src/pages/market/ProductNewReviewPage.jsx
+++ src/pages/market/ProductNewReviewPage.jsx
@@ -1,10 +1,16 @@
 import HeaderIcon from "@components/HeaderIcon";
-import { useEffect } from "react";
+import NewPost from "@components/NewPost";
+import { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate, useOutletContext } from "react-router-dom";
 
 export default function ProductNewReviewPage() {
   const { setHeaderContents } = useOutletContext();
   const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const isBoard = false;
+
+  const formRef = useRef(null);
 
   useEffect(() => {
     setHeaderContents({
@@ -19,5 +25,18 @@ export default function ProductNewReviewPage() {
     });
   }, []);
 
-  return <h1>Product New Review Page</h1>;
+  const check = (item) => {
+    console.log(item);
+  };
+
+  return (
+    <>
+      <NewPost
+        isBoard={isBoard}
+        formRef={formRef}
+        handleSubmit={handleSubmit(check)}
+        register={register}
+      ></NewPost>
+    </>
+  );
 }
