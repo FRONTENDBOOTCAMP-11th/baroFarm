0180a7411afec9f7e5340efce84a07cd1ed640fc
Taiho1998 <tgo12323@gmail.com>
2025-01-03 15:44:17 +0900

    feature/DB상의 community 타입의 post 조회 기능 작성

--- src/pages/board/BoardPage.jsx
+++ src/pages/board/BoardPage.jsx
@@ -1,5 +1,7 @@
 import HeaderIcon from "@components/HeaderIcon";
 import BoardPageDetail from "@pages/board/BoardPageDetail";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useOutletContext } from "react-router-dom";
 
@@ -20,6 +22,36 @@ export default function BoardPage() {
       ),
     });
   }, []);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios.get("https://11.fesp.shop/posts?type=community", {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+          "client-id": "final04",
+        },
+      }),
+    select: (res) => res.data.item,
+    staleTime: 1000 * 10,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="mt-0 mx-auto text-center">
+        로딩중... <br />
+        잠시만 기다려주세요
+      </div>
+    );
+  }
+
+  console.log(data);
+
+  const boards = data.map((item) => (
+    <BoardPageDetail key={item._id} item={item} />
+  ));
+
   return (
     <div className="relative mx-5">
       <div className="flex my-2 items-center bg-green1 rounded-md">
@@ -35,8 +67,7 @@ export default function BoardPage() {
         </p>
       </div>
       <div className="h-[7px] bg-gray1 -mx-5"></div>
-      <BoardPageDetail />
-      <BoardPageDetail />
+      {boards}
       <Link
         to="new"
         className="fixed right-[calc(50%-155px)] bottom-[130px] w-[40px] h-[40px] rounded-full shadow-bottom"
--- src/pages/board/BoardPageDetail.jsx
+++ src/pages/board/BoardPageDetail.jsx
@@ -1,28 +1,34 @@
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export default function BoardPageDetail() {
+BoardPageDetail.propTypes = {
+  item: PropTypes.shape(),
+};
+
+export default function BoardPageDetail({ item }) {
+  console.log(item.user.image);
   return (
     <div className="relative">
       <Link to={"1"}>
         <div className="flex flex-row mt-5 items-center">
           <img
-            src="/images/profile/Profile_sample_1.jpg"
+            src={`https://11.fesp.shop${item.user.image}`}
             alt="ProfileImage"
             className="w-6 h-6 rounded-full"
           />
-          <span className="mx-[5px] text-sm">온도감</span>
+          <span className="mx-[5px] text-sm">{item.user.name}</span>
 
-          <span className="ml-auto text-xs self-start">댓글 1개</span>
+          <span className="ml-auto text-xs self-start">
+            댓글 {item.repliesCount}개
+          </span>
         </div>
-        <div className="mx-[5px] mt-[30px]">
-          요즘 토마토가 또 철이네요~ 우리 집에서 기른 토마토로 만든...
-        </div>
+        <div className="mx-[5px] mt-[30px]">{item.content}</div>
         <img
           className="relative mt-10 rounded-md"
-          src="/images/sample/food.svg"
+          src={`https://11.fesp.shop${item.image}`}
         />
         <div className="text-[10px] text-gray4 text-right mb-5 mt-1">
-          16분 전
+          {item.updatedAt}
         </div>
       </Link>
       <div className="h-[7px] bg-gray1 -mx-5"></div>
