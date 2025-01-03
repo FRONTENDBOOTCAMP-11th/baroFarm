a49283737543b01029da387e6e568120ab9cc69a
Taiho1998 <tgo12323@gmail.com>
2025-01-03 16:30:51 +0900

    feature/시간 오표기 수정

--- src/pages/board/BoardPageDetail.jsx
+++ src/pages/board/BoardPageDetail.jsx
@@ -12,9 +12,9 @@ export default function BoardPageDetail({ item }) {
       const pastDate = new Date(inputDate);
       const minDiff = Math.floor((now - pastDate) / (1000 * 60));
       if (minDiff < 1) return "방금 전";
-      if (minDiff <= 60) return `${minDiff}분 전`;
-      if (minDiff <= 1440) return `${minDiff / 60}시간 전`;
-      if (minDiff <= 2880) return `${minDiff / 1440}일 전`;
+      if (minDiff < 60) return `${minDiff}분 전`;
+      if (minDiff < 1440) return `${parseInt(minDiff / 60)}시간 전`;
+      if (minDiff < 2880) return `${parseInt(minDiff / 1440)}일 전`;
 
       // 이틀 이상인 경우에는 날짜를 표시
       return pastDate.toLocaleString();
