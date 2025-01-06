b7279e888172a2a752a4f04a38ad4ebd26e9af74
Taiho1998 <tgo12323@gmail.com>
2025-01-06 11:15:16 +0900

    feature/댓글 아이템 분리 및 댓글 수가 0일 경우 출력되는 페이지 수정

--- src/pages/board/Comment.jsx
+++ src/pages/board/Comment.jsx
@@ -1,3 +1,4 @@
+import CommentItem from "@pages/board/CommentItem";
 import PropTypes from "prop-types";
 
 Comment.propTypes = {
@@ -8,27 +9,13 @@ export default function Comment({ repliesCount }) {
   return (
     <section className="pt-5">
       <span className="font-semibold">댓글 ({repliesCount})</span>
-      <div className="px-[15px] ">
-        <div className="flex flex-row mt-5">
-          <img
-            src="/images/profile/Profile_sample_1.jpg"
-            alt="ProfileImage"
-            className="w-6 h-6 rounded-full border"
-          />
-          <span className="mx-[5px] text-sm">떡보369</span>
-          <span className="text-[10px] mt-[6px] ml-auto text-gray4">
-            3분 전
-          </span>
+      {!repliesCount && (
+        <div className="h-[85px] flex justify-center items-center">
+          아직 댓글이 없습니다!
         </div>
-        <div className="flex pb-5 border-b-[1px] border-gray3/50">
-          <div className="mt-3 text-xs text-gray5">
-            와 보기만 해도 정말 맛있겠네요!
-          </div>
-          <span className="ml-auto text-xs mt-auto">
-            <button>수정</button> | <button>삭제</button>
-          </span>
-        </div>
-      </div>
+      )}
+      {/* <CommentItem />
+      <CommentItem /> */}
     </section>
   );
 }
---
+++ src/pages/board/CommentItem.jsx
@@ -0,0 +1,23 @@
+export default function CommentItem() {
+  return (
+    <div className="px-[15px] ">
+      <div className="flex flex-row mt-5">
+        <img
+          src="/images/profile/Profile_sample_1.jpg"
+          alt="ProfileImage"
+          className="w-6 h-6 rounded-full border"
+        />
+        <span className="mx-[5px] text-sm">떡보369</span>
+        <span className="text-[10px] mt-[6px] ml-auto text-gray4">3분 전</span>
+      </div>
+      <div className="flex pb-5 border-b-[1px] border-gray3/50">
+        <div className="mt-3 text-xs text-gray5">
+          와 보기만 해도 정말 맛있겠네요!
+        </div>
+        <span className="ml-auto text-xs mt-auto">
+          <button>수정</button> | <button>삭제</button>
+        </span>
+      </div>
+    </div>
+  );
+}
