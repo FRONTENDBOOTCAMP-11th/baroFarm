7e36f172dd5431952cd831cbf4bed0ec39bf4588
Taiho1998 <tgo12323@gmail.com>
2025-01-06 11:10:22 +0900

    feature/댓글 영역 정리

--- src/pages/board/BoardDetailPage.jsx
+++ src/pages/board/BoardDetailPage.jsx
@@ -75,58 +75,7 @@ export default function BoardDetailPage() {
       <div className="text-right text-xs">
         <button>수정</button> | <button>삭제</button>
       </div>
-      <div className="pt-5">
-        <span className="font-semibold">댓글 ({repliesCount})</span>
-
-        <Comment />
-        <Comment />
-        {/* <div className="px-[15px] ">
-          <div className="flex flex-row mt-5">
-            <img
-              src="/images/profile/Profile_sample_1.jpg"
-              alt="ProfileImage"
-              className="w-6 h-6 rounded-full border"
-            />
-            <span className="mx-[5px] text-sm">떡보369</span>
-            <span className="text-[10px] mt-[6px] ml-auto text-gray4">
-              3분 전
-            </span>
-          </div>
-          <div className="flex pb-5 border-b-[1px] border-gray3/50">
-            <div className="mt-3 text-xs text-gray5">
-              와 보기만 해도 정말 맛있겠네요!
-            </div>
-            <span className="ml-auto text-xs mt-auto">
-              <button>수정</button> | <button>삭제</button>
-            </span>
-          </div>
-        </div>
-        <div className="px-[15px] ">
-          <div className="flex flex-row mt-5">
-            <img
-              src="/images/profile/Profile_sample_2.jpg"
-              alt="ProfileImage"
-              className="w-6 h-6 rounded-full border"
-            />
-            <span className="mx-[5px] text-sm">삼행시 빌런</span>
-            <span className="text-[10px] mt-[6px] ml-auto text-gray4">
-              15분 전
-            </span>
-          </div>
-          <div className="flex pb-5 border-b-[1px] border-gray3/50">
-            <div className="mt-3 text-xs text-gray5">
-              토: 토마토
-              <br />
-              마: 마지막으로 말하지만
-              <br />
-              토: 토-메이-토입니다
-            </div>
-            <span className="ml-auto text-xs mt-auto">
-              <button>수정</button> | <button>삭제</button>
-            </span>
-          </div>
-        </div> */}
-      </div>
+      <Comment repliesCount={repliesCount} />
 
       <div className="h-[65px] flex items-center px-5 -mx-5">
         <input
--- src/pages/board/Comment.jsx
+++ src/pages/board/Comment.jsx
@@ -1,23 +1,34 @@
-export default function Comment() {
+import PropTypes from "prop-types";
+
+Comment.propTypes = {
+  repliesCount: PropTypes.number.isRequired,
+};
+
+export default function Comment({ repliesCount }) {
   return (
-    <div className="px-[15px] ">
-      <div className="flex flex-row mt-5">
-        <img
-          src="/images/profile/Profile_sample_1.jpg"
-          alt="ProfileImage"
-          className="w-6 h-6 rounded-full border"
-        />
-        <span className="mx-[5px] text-sm">떡보369</span>
-        <span className="text-[10px] mt-[6px] ml-auto text-gray4">3분 전</span>
-      </div>
-      <div className="flex pb-5 border-b-[1px] border-gray3/50">
-        <div className="mt-3 text-xs text-gray5">
-          와 보기만 해도 정말 맛있겠네요!
+    <section className="pt-5">
+      <span className="font-semibold">댓글 ({repliesCount})</span>
+      <div className="px-[15px] ">
+        <div className="flex flex-row mt-5">
+          <img
+            src="/images/profile/Profile_sample_1.jpg"
+            alt="ProfileImage"
+            className="w-6 h-6 rounded-full border"
+          />
+          <span className="mx-[5px] text-sm">떡보369</span>
+          <span className="text-[10px] mt-[6px] ml-auto text-gray4">
+            3분 전
+          </span>
         </div>
-        <span className="ml-auto text-xs mt-auto">
-          <button>수정</button> | <button>삭제</button>
-        </span>
+        <div className="flex pb-5 border-b-[1px] border-gray3/50">
+          <div className="mt-3 text-xs text-gray5">
+            와 보기만 해도 정말 맛있겠네요!
+          </div>
+          <span className="ml-auto text-xs mt-auto">
+            <button>수정</button> | <button>삭제</button>
+          </span>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
