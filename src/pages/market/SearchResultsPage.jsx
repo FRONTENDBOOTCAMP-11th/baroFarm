42662f70d30da33c1371e8e76feb2b4514df39c4
eunjinim <eunjee2058@gmail.com>
2025-01-08 16:16:25 +0900

    feat: 검색 결과 페이지 기본 구현

--- src/pages/market/SearchResultsPage.jsx
+++ src/pages/market/SearchResultsPage.jsx
@@ -1,73 +1,62 @@
 // React와 라우터 관련
 import { useEffect } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext, useSearchParams } from "react-router-dom";
 
 // 컴포넌트
 import HeaderIcon from "@components/HeaderIcon";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosInstance from "@hooks/useAxiosInstance";
 import Products from "@components/Products";
 
-// 이미지
-import productImage1 from "/images/Sample1.svg";
-import productImage2 from "/images/Sample2.svg";
-
-const productsData = [
-  {
-    id: 1,
-    image: productImage1,
-    title: "온도감",
-    content: "촉촉함이 다른 카스테라 5종...",
-    sale: "92%",
-    price: "14,900원",
-    rate: "⭐️ 4.9",
-    review: "(2,210)",
-  },
-  {
-    id: 2,
-    image: productImage2,
-    title: "강아지",
-    content: "강아지 귀여워",
-    sale: "12%",
-    price: "24,900원",
-    rate: "⭐️ 3.9",
-    review: "(6,210)",
-  },
-  {
-    id: 3,
-    image: productImage1,
-    title: "햄스터",
-    content: "햄스터 귀여워",
-    sale: "2%",
-    price: "4,900원",
-    rate: "⭐️ 0.9",
-    review: "(210)",
-  },
-  {
-    id: 4,
-    image: productImage2,
-    title: "강아지",
-    content: "강아지 귀여워",
-    sale: "2%",
-    price: "4,900원",
-    rate: "⭐️ 0.9",
-    review: "(210)",
-  },
-];
-
 export default function SearchResultsPage() {
+  // URL의 쿼리 파라미터를 가져오기
+  const [searchParams] = useSearchParams();
+  // URL 쿼리 파라미터에서 "keyword" 값을 추출 (예: ?keyword=귤 → "귤")
+  const keyword = searchParams.get("keyword");
+  const axios = useAxiosInstance();
+
   const { setHeaderContents } = useOutletContext();
   const navigate = useNavigate();
 
   useEffect(() => {
     setHeaderContents({
-      leftChild: <HeaderIcon name="back" onClick={() => navigate("/")} />,
-      title: "검색어",
+      leftChild: <HeaderIcon name="back" onClick={() => navigate("/search")} />,
+      title: keyword,
     });
   }, []);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["searchProducts", keyword],
+    queryFn: () => axios.get("/products", { params: { keyword: keyword } }),
+    select: (res) => res.data.item,
+    staleTime: 1000 * 60, // 1분
+    // !! 연산자: keyword의 값을 boolean으로 변환
+    // null, undefined, 빈 문자열 → false (쿼리 미실행)
+    // 실제 문자열 존재 → true (쿼리 실행)
+    // enabled: !!keyword,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="mt-0 mx-auto text-center">
+        로딩중... <br />
+        잠시만 기다려주세요
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="mt-0 mx-auto text-center">
+        에러가 발생했습니다. <br />
+        잠시 후 다시 시도해주세요.
+      </div>
+    );
+  }
 
   return (
     <div>
       <div className="p-5 flex items-center font-semibold">
-        <h2>총 4개</h2>
+        <h2>총 {data.length}개</h2>
         <div className="ml-auto text-sm">
           <select
             className=" text-center bg-gray2 rounded-lg py-1 ps-3 pe-6 appearance-none focus:outline-none cursor-pointer
@@ -86,14 +75,14 @@ export default function SearchResultsPage() {
         </div>
       </div>
       {/* 검색 결과 없을 때와 있을 때의 조건부 렌더링 */}
-      {productsData.length === 0 ? (
+      {data.length === 0 ? (
         <div className="p-5 text-sm text-center font-medium mt-3">
           <img className="block m-auto mb-2" src="/icons/icon_sad.svg" alt="검색 결과 없음을 나타내는 슬픈 표정" />
           <p>입력하신 검색어의 결과를 찾을 수 없습니다.</p>
           <p>다른 검색어로 시도하시거나 맞춤법을 확인해주세요.</p>
         </div>
       ) : (
-        <Products productsData={productsData} />
+        <Products productsData={data} />
       )}
     </div>
   );
