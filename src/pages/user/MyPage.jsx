c142facbda98f13165fe7e03c8199f2d305c4bb9
Taiho1998 <tgo12323@gmail.com>
2025-01-09 13:44:05 +0900

    feature/userStore의 _id를 이용하여 유저 정보 불러온 후 페이지에 적용

--- src/pages/user/MyPage.jsx
+++ src/pages/user/MyPage.jsx
@@ -1,4 +1,6 @@
 import HeaderIcon from "@components/HeaderIcon";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useQuery } from "@tanstack/react-query";
 import useUserStore from "@zustand/useUserStore";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useOutletContext } from "react-router-dom";
@@ -6,10 +8,12 @@ import { Link, useNavigate, useOutletContext } from "react-router-dom";
 export default function MyPage() {
   const { setHeaderContents } = useOutletContext();
   const navigate = useNavigate();
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
   // zustand store에서 유저 상태 가져옴
   const user = useUserStore((store) => store.user);
+
+  const axios = useAxiosInstance();
 
   useEffect(() => {
     setHeaderContents({
@@ -25,7 +29,7 @@ export default function MyPage() {
 
   /// store에서 user 상태를 초기화하는 함수 가져오기
   const resetUser = useUserStore((store) => store.resetUser);
-  
+
   const logoutClick = () => {
     //로그아웃 시 데이터 삭제
     // setData(null); - 태호님 작성
@@ -40,6 +44,20 @@ export default function MyPage() {
     navigate("/users/login");
   };
 
+  const { data } = useQuery({
+    queryKey: ["user", user?._id],
+    queryFn: () => axios.get(`/users/${user._id}`),
+    select: (res) => res.data.item,
+    staleTime: 1000 * 10,
+  });
+
+  if (!data && user) {
+    //이 아래에는 로딩 페이지
+    return;
+  }
+
+  console.log(data);
+
   return (
     <div className="pt-[18px] px-5 mb-[70px]">
       <div className="h-auto pb-4">
@@ -52,12 +70,23 @@ export default function MyPage() {
                 loading="lazy"
               />
               <div>
-                <p className="text-gray5/50 text-[12px] leading-[14px]">판매자</p>
-                <h2 className="text-[16px] leading-[18px] mt-[4px]">{user.name}님! 어서오세요</h2>
+                <p className="text-gray5/50 text-[12px] leading-[14px]">
+                  {data.type == "seller" ? "판매자" : "구매자"}
+                </p>
+                <h2 className="text-[16px] leading-[18px] mt-[4px]">
+                  {user.name}님! 어서오세요
+                </h2>
               </div>
-              <button onClick={logoutClick} className="flex ml-auto h-fit items-center text-[14px]">
+              <button
+                onClick={logoutClick}
+                className="flex ml-auto h-fit items-center text-[14px]"
+              >
                 로그아웃
-                <img src="/icons/icon_forward.svg" className="h-4 ml-2" alt="profileDetail icon" />
+                <img
+                  src="/icons/icon_forward.svg"
+                  className="h-4 ml-2"
+                  alt="profileDetail icon"
+                />
               </button>
             </>
           )}
@@ -69,11 +98,20 @@ export default function MyPage() {
                 loading="lazy"
               />
               <div>
-                <h2 className="text-[16px] leading-[18px] mt-[4px]">게스트님! 어서오세요</h2>
+                <h2 className="text-[16px] leading-[18px] mt-[4px]">
+                  게스트님! 어서오세요
+                </h2>
               </div>
-              <button onClick={loginClick} className="flex ml-auto h-fit items-center text-[14px]">
+              <button
+                onClick={loginClick}
+                className="flex ml-auto h-fit items-center text-[14px]"
+              >
                 로그인하기
-                <img src="/icons/icon_forward.svg" className="h-4 ml-2" alt="profileDetail icon" />
+                <img
+                  src="/icons/icon_forward.svg"
+                  className="h-4 ml-2"
+                  alt="profileDetail icon"
+                />
               </button>
             </>
           )}
@@ -92,8 +130,12 @@ export default function MyPage() {
             >
               판매 내역
             </Link>
-            <Link to={"/users/myboard"} className="flex justify-center items-center flex-1 text-center h-[50px]">
-              작성한 글 <span className="text-btn-primary ml-1">3건</span>
+            <Link
+              to={"/users/myboard"}
+              className="flex justify-center items-center flex-1 text-center h-[50px]"
+            >
+              작성한 글{" "}
+              <span className="text-btn-primary ml-1">{data.posts}건</span>
             </Link>
           </div>
         )}
@@ -101,13 +143,27 @@ export default function MyPage() {
       <div className="h-[7px] bg-gray1 mx-[-20px]"></div>
       <div className="h-[152px] pt-[18px]">
         <h2 className="text-base leading-[19px]">구매 정보</h2>
-        <Link to={"/users/recent"} className="flex items-center text-[14px] mt-[27px]">
+        <Link
+          to={"/users/recent"}
+          className="flex items-center text-[14px] mt-[27px]"
+        >
           최근 본 상품
-          <img src="/icons/icon_forward.svg" className="h-[16px] ml-auto" alt="recentProduct icon" />
+          <img
+            src="/icons/icon_forward.svg"
+            className="h-[16px] ml-auto"
+            alt="recentProduct icon"
+          />
         </Link>
-        <Link to={"/users/bookmarks"} className="flex items-center text-[14px] mt-[24px]">
+        <Link
+          to={"/users/bookmarks"}
+          className="flex items-center text-[14px] mt-[24px]"
+        >
           찜한 상품
-          <img src="/icons/icon_forward.svg" className="h-[16px] ml-auto" alt="likedProduct icon" />
+          <img
+            src="/icons/icon_forward.svg"
+            className="h-[16px] ml-auto"
+            alt="likedProduct icon"
+          />
         </Link>
       </div>
       {/* 해당 영역은 로그아웃 상태일 시 사용을 필요로 하지 않음 */}
@@ -116,9 +172,16 @@ export default function MyPage() {
           <div className="h-[7px] bg-gray1 mx-[-20px]"></div>
           <div className="h-[109px] pt-[18px] ">
             <h2 className="text-base leading-[19px]">판매 정보</h2>
-            <Link to={"/product/new"} className="flex items-center text-[14px] mt-[27px] mb-[24px]">
+            <Link
+              to={"/product/new"}
+              className="flex items-center text-[14px] mt-[27px] mb-[24px]"
+            >
               상품 등록
-              <img src="/icons/icon_forward.svg" className="h-[16px] ml-auto" alt="addProduct icon" />
+              <img
+                src="/icons/icon_forward.svg"
+                className="h-[16px] ml-auto"
+                alt="addProduct icon"
+              />
             </Link>
           </div>
           <div className="h-[7px] bg-gray1 mx-[-20px]"></div>
@@ -130,11 +193,19 @@ export default function MyPage() {
               state={{ user: data }}
             >
               내 정보 보기
-              <img src="/icons/icon_forward.svg" className="h-[16px] ml-auto" alt="addProduct icon" />
+              <img
+                src="/icons/icon_forward.svg"
+                className="h-[16px] ml-auto"
+                alt="addProduct icon"
+              />
             </Link>
             <Link to={""} className="flex items-center text-[14px] my-[24px]">
               탈퇴하기
-              <img src="/icons/icon_forward.svg" className="h-[16px] ml-auto" alt="addProduct icon" />
+              <img
+                src="/icons/icon_forward.svg"
+                className="h-[16px] ml-auto"
+                alt="addProduct icon"
+              />
             </Link>
           </div>
         </>
