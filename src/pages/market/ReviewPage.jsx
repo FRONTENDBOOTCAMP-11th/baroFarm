1a940ea4fb11eb58cebda9b0b26f357033d413ae
Saebom <spring180@naver.com>
2025-01-14 16:54:57 +0900

    feat: 별점순 정렬

--- src/pages/market/ReviewPage.jsx
+++ src/pages/market/ReviewPage.jsx
@@ -29,28 +29,39 @@ export default function ReviewPage() {
   }, []);
 
   const {
-    data: product,
+    data: reviewData,
     isLoading,
     isError,
   } = useQuery({
     queryKey: ["product", _id, "reviews"],
     queryFn: async () => {
-      const response = await instance.get(`/products/${_id}`);
+      if (sortOrder === "best") {
+        const end = `{"rating": -1}`;
+      } else {
+        const end = `{"createdAt": -1}`;
+      }
+      const response = await instance.get(
+        `/replies/products/${_id}?sort={"rating": -1}`
+      );
       return response.data.item;
     },
   });
 
   if (isLoading) return <div>Loading...</div>;
-  if (isError || !product) return <div>Error loading product</div>;
+  if (isError || !reviewData) return <div>Error loading product</div>;
+
+  const ratings = reviewData.map((review) => review.rating);
+  const totalRating =
+    ratings.reduce((acc, curr) => acc + curr, 0) / ratings.length;
 
   return (
     <>
       <section className="p-5 border-b-[1px] border-b-gray2">
         <p className="font-medium pb-2">전체 구매자 평점</p>
         <span className="font-semibold pr-2">
-          {Array(Math.floor(product.rating)).fill("⭐️")}
+          {Array(Math.floor(totalRating)).fill("⭐️")}
         </span>
-        <span className="font-extrabold">{product.rating.toFixed(1)}</span>
+        <span className="font-extrabold">{totalRating.toFixed(1)}</span>
       </section>
 
       <section className="p-5 border-b-8 border-b-gray1">
@@ -65,7 +76,7 @@ export default function ReviewPage() {
         </Link> */}
         </div>
         <div className="flex overflow-x-auto gap-3 scrollbar-hide pt-5">
-          {product.replies
+          {reviewData
             .filter((review) => review.extra && review.extra.image)
             .map((review) => (
               <PhotoReviewItem key={review._id} image={review.extra.image} />
@@ -74,14 +85,14 @@ export default function ReviewPage() {
       </section>
 
       <section className="py-5">
-        <p className="font-bold pl-5 pb-1">후기 {product.replies.length}개</p>
-        {/* <button
+        <p className="font-bold pl-5 pb-1">후기 {reviewData.length}개</p>
+        <button
           className={`pl-5 text-sm font-semibold ${
             sortOrder === "best" ? "text-bg-primary" : "text-gray4"
           }`}
           onClick={() => handleSort("best")}
         >
-          평점순
+          별점순
         </button>
         <button
           className={`pl-2 text-sm font-semibold ${
@@ -90,12 +101,12 @@ export default function ReviewPage() {
           onClick={() => handleSort("new")}
         >
           최신순
-        </button> */}
-        {product.replies.map((reply) => (
+        </button>
+        {reviewData.map((reply) => (
           <ReviewItem
             key={reply._id}
             reply={reply}
-            productName={product.name}
+            productName={reply.product.name}
           />
         ))}
       </section>
