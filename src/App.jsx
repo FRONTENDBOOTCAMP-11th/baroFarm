93c9dfe2296e9cd446f6aac004ff41d281820731
eunjinim <eunjee2058@gmail.com>
2025-01-15 10:10:44 +0900

    refactor: 카카오 SDK 초기화 로직 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
 import { HelmetProvider } from "react-helmet-async";
-import { useEffect } from "react";
 
 function App() {
-  // 카카오 SDK 초기화
-  // - 카카오 로그인 등의 기능을 사용하기 위해 필수적인 과정
-  // - 앱 시작 시 최초 1회만 실행되어야 함
-  useEffect(() => {
-    // SDK가 초기화되지 않은 경우에만 초기화 진행
-    // window.Kakao를 사용하는 이유
-    // - React 컴포넌트(.jsx) 파일은 독립적인 공간을 가지고 있어서
-    // - index.html에서 추가한 카카오 SDK를 바로 찾을 수 없음
-    // - window(브라우저 전역 객체)를 통해 카카오 SDK에 접근해야 함
-    if (!window.Kakao.isInitialized()) {
-      // VITE_KAKAO_JS_KEY: 카카오 Developer에서 발급받은 JavaScript 키
-      window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
-      console.log("카카오 초기화 여부: ", window.Kakao.isInitialized());
-    }
-  }, []);
-
   return (
     <>
       <HelmetProvider>
--- src/pages/user/LoginPage.jsx
+++ src/pages/user/LoginPage.jsx
@@ -111,6 +111,17 @@ export default function LoginPage() {
   });
 
   const handleKakaoLogin = () => {
+    // SDK가 초기화되지 않은 경우에만 초기화 진행
+    // window.Kakao를 사용하는 이유
+    // - React 컴포넌트(.jsx) 파일은 독립적인 공간을 가지고 있어서
+    // - index.html에서 추가한 카카오 SDK를 바로 찾을 수 없음
+    // - window(브라우저 전역 객체)를 통해 카카오 SDK에 접근해야 함
+    if (!window.Kakao.isInitialized()) {
+      // VITE_KAKAO_JS_KEY: 카카오 Developer에서 발급받은 JavaScript 키
+      window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
+      console.log("카카오 초기화 여부: ", window.Kakao.isInitialized());
+    }
+
     window.Kakao.Auth.authorize({
       // window.location.origin: 현재 웹사이트의 도메인 주소를 가져옴 => 환경에 따라 자동으로 적절한 도메인이 적용됨
       redirectUri: `${window.location.origin}/users/login/kakao`,
