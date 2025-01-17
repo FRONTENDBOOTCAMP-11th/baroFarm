7c41d62e423ca0c4f971d367f55c1b0ad9525050
Saebom <spring180@naver.com>
2025-01-17 11:39:20 +0900

    fix: 포토 리뷰에 border 추가

--- src/components/PhotoReviewItem.jsx
+++ src/components/PhotoReviewItem.jsx
@@ -2,7 +2,7 @@ export default function PhotoReviewItem({ image }) {
   return (
     <img
       src={`https://11.fesp.shop${image}`}
-      className="w-[100px] h-[100px] object-cover rounded-md items-center"
+      className="w-[100px] h-[100px] object-cover rounded-md items-center border"
     />
   );
 }
