01878e7b3a80192058354af5f5f53654086102a2
Taiho1998 <tgo12323@gmail.com>
2025-01-20 01:20:19 +0900

    fix/게시판 페이지 Pagination 설정

---
+++ src/pages/board/Pagination.jsx
@@ -0,0 +1,31 @@
+export default function Pagination({ page, handlePageChange, totalPages }) {
+  return (
+    <>
+      <button
+        disabled={page === 1}
+        onClick={() => handlePageChange(page - 1)}
+        className="px-3 py-1 mx-1 text-sm bg-gray2 rounded-md disabled:opacity-50"
+      >
+        이전
+      </button>
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index}
+          onClick={() => handlePageChange(index + 1)}
+          className={`px-3 py-1 mx-1 text-sm rounded-md ${
+            page === index + 1 ? "bg-btn-primary text-white" : "bg-gray2"
+          }`}
+        >
+          {index + 1}
+        </button>
+      ))}
+      <button
+        disabled={page === totalPages}
+        onClick={() => handlePageChange(page + 1)}
+        className="px-3 py-1 mx-1 text-sm bg-gray2 rounded-md disabled:opacity-50"
+      >
+        다음
+      </button>
+    </>
+  );
+}
